8ee5874adb98cd5daf4ec785685a433568150546
David Nino <juan-galvis@outlook.com>
2025-12-30 17:22:06 -0500

    feat: update NavBar and Layout components for improved navigation and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan-dark': '#067f75',
+        'yellow-custom': '#FFC519',
+        'cyan-light': '#0E8D81',
+        'white-bg': '#FEFEFE',
+      },
+    },
   },
   plugins: [],
 }
